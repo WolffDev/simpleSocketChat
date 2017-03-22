71880a40930dd62ea6bd92336c1465e00713be46
David Wolff <info@davidwolff.dk>
2017-03-22 22:18:22 +0100

    downgraded all modules

--- server.js
+++ server.js
@@ -1,32 +1,14 @@
-var http = require("http"),
-	express = require("express"),
-	socketIo = require("socket.io");
+const io = require('socket.io');
 
-const app = express();
-const port = process.env.PORT || 3000;
-const server = new http.Server(app);
-const io = socketIo(server);
+const app = express()
+  , server = require('http').createServer(app)
+  , io = io.listen(server)
+  , port = process.env.PORT || 3000;
 
-var history = [];
-console.log("All const and vars are required");
+server.listen(port);
 
-io.on('connection', socket => {
-	console.log("A new user just connected");
-
-	socket.on("chat:add", data => {
-		console.log(data);
-		history.push(data);
-		io.emit("chat:added", data);
-	});
-	
-	socket.on('disconnect', () => {
-		console.log("A user disconnected");
-	});
+io.sockets.on('connection', function (socket) {
+  socket.on('chat:add', function (data) {
+    console.log(data);
+  });
 });
-
-
-server.listen(port, () => {
-	console.log(`Server started on http://localhost:${port}`);
-});
-
-module.exports = server;
